52779f9624343c7c82d2d19834a7f688f1a5fffe
ADVALAKI <alpeshvalaki@gmail.com>
2017-11-17 15:41:41 +0530

    Update sale.py
    We can get credit and debit directly from partner.

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -16,27 +16,27 @@ class SaleOrder(models.Model):
     def check_limit(self):
         self.ensure_one()
         partner = self.partner_id
-        moveline_obj = self.env['account.move.line']
-        movelines = moveline_obj.search(
-            [('partner_id', '=', partner.id),
-             ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
-             ('full_reconcile_id', '=', False)]
-        )
+#         moveline_obj = self.env['account.move.line']
+#         movelines = moveline_obj.search(
+#             [('partner_id', '=', partner.id),
+#              ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable']),
+#              ('full_reconcile_id', '=', False)]
+#         )
 
-        debit, credit = 0.0, 0.0
+#         debit, credit = 0.0, 0.0
         today_dt = datetime.strftime(datetime.now().date(), DF)
-        for line in movelines:
-            if line.date_maturity < today_dt:
-                credit += line.debit
-                debit += line.credit
+#         for line in movelines:
+#             if line.date_maturity < today_dt:
+#                 credit += line.debit
+#                 debit += line.credit
 
-        if (credit - debit + self.amount_total) > partner.credit_limit:
+        if (partner.credit - partner.debit + self.amount_total) > partner.credit_limit:
             if not partner.over_credit:
                 msg = 'Can not confirm Sale Order,Total mature due Amount ' \
                       '%s as on %s !\nCheck Partner Accounts or Credit ' \
-                      'Limits !' % (credit - debit, today_dt)
+                      'Limits !' % (partner.credit - partner.debit, today_dt)
                 raise UserError(_('Credit Over Limits !\n' + msg))
-            partner.write({'credit_limit': credit - debit + self.amount_total})
+            partner.write({'credit_limit': partner.credit - partner.debit + self.amount_total})
         return True
 
     @api.multi
